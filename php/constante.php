66ceeef118dc669d70d2dc034571c57b122739fe
alunoenzoye <enzo.eto.senai404@gmail.com>
2024-08-07 15:31:58 -0300

    Add files via upload

---
+++ php/constante.php
@@ -0,0 +1,14 @@
+<?php
+    //Definir as constantes
+
+    define('BD_URL', 'endereco_bd_dev');
+    define('BD_USUARIO', 'usuario_dev');
+    define('BD_SENHA', 'senha_dev');
+
+
+    //Utilizar o echo para exibir as constantes
+
+    echo BD_URL . '<br/>';
+    echo BD_USUARIO . '<br/>';
+    echo BD_SENHA . '<br/>';
+?>
---
+++ php/ifelse.php
@@ -0,0 +1,12 @@
+<?php
+
+$soma1 = 9;
+$soma2 = 9;
+
+if ($soma1 == $soma2) {
+    echo 'Os dois números são iguais';
+} else {
+    echo 'Os dois números são diferentes';
+}
+
+?>
