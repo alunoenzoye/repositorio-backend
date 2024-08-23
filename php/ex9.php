98365b0f11ed00017de9a3dcfc9903af20f2ddee
alunoenzoye <enzo.eto.senai404@gmail.com>
2024-08-23 09:30:43 -0300

    Add files via upload

---
+++ php/ex9.php
@@ -0,0 +1,10 @@
+<?php
+
+//Iterar de 1 a 50, exibindo a raiz quadrada de cada número.
+for ($i = 1; $i <= 50; $i++) {
+    $raiz = sqrt($i);
+
+    echo "A raíz quadrada de $i é $raiz.<br>";
+}
+
+?>
