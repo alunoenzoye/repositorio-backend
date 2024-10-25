f88f33c429fbf96d38abcf9b6a1079e92f670684
alunoenzoye <enzo.eto.senai404@gmail.com>
2024-10-25 10:52:51 -0300

    Delete xampp/htdocs/app_help_desk directory

--- xampp/htdocs/app_help_desk/login.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-// echo $_SESSION['autenticado'];
-// Verificar se o usuário está autenticado através da SESSION
-if (!(isset($_SESSION['autenticado'])) or !($_SESSION['autenticado'])) {
-  header('location: pagina_login.php?login=erro2');
-}
