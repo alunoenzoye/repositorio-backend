8dc176abdd023d828e7e74f1135a5779f20bd9ca
alunoenzoye <enzo.eto.senai404@gmail.com>
2024-10-25 09:23:56 -0300

    Delete app_help_desk directory

--- app_help_desk/lib/debug.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Decorarar uma função com o echo
-function pre_decorator($func) {
-    echo "<pre>";
-    $func();
-    echo "</pre>";
-}
--- app_help_desk/login.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-// echo $_SESSION['autenticado'];
-// Verificar se o usuário está autenticado através da SESSION
-if (!(isset($_SESSION['autenticado'])) or !($_SESSION['autenticado'])) {
-  header('location: index.php?login=erro2');
-}
